262deace9660a1286b45fa7b34d83043b9e2c39b
Pratik Sawant <119058935+pratik9892@users.noreply.github.com>
2026-04-21 14:22:53 +0530

    feat: fix cookie err

--- src/socket.js
+++ src/socket.js
@@ -6,6 +6,7 @@ export const getSocket = () => {
   if (!socket) {
     socket = io(import.meta.env.VITE_BACKEND_URL, {
       transports: ['websocket'],
+      withCredentials : true
     });
   }
   return socket;
